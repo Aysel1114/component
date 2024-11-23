cf963bf7671e4087e0d25feab3541565d6ea77f8
Tarverdiyeva Aysel <t.aysel2017@gmail.com>
2024-11-23 15:16:55 +0400

    second commit

--- vite-project/src/Wine.jsx
+++ vite-project/src/Wine.jsx
@@ -1,12 +1,16 @@
 import { Component } from "react";
 
 export default class Wine extends Component {
+    clickHandler = (event) => {
+        console.log(event.target);
+    }
     render() {
         return (
             <div>
                 <h3>{this.props.title}</h3>
                 <p>{this.props.description}</p>
                 <p>{this.props.rating}</p>
+                <button onClick={this.clickHandler}>Button click!</button>
             </div>
         )
     }
