3527a0f5bb8c0b7d595862270d885c6a3908c1e5
Tarverdiyeva Aysel <t.aysel2017@gmail.com>
2024-11-23 16:31:04 +0400

    third commit

--- vite-project/src/Wine.jsx
+++ vite-project/src/Wine.jsx
@@ -1,16 +1,34 @@
 import { Component } from "react";
+import './App.css'
 
 export default class Wine extends Component {
-    clickHandler = (event) => {
-        console.log(event.target);
+    // clickHandler = (event) => {
+    //     console.log(event.target);
+    // }
+    state = {
+        click: false,
+        message: "Clicked here",
+    };
+    clicked = () => {
+        this.setState({ message: "Clicked", click: true });
     }
     render() {
+        const {message, click} = this.state;
+        let className = "message-button";
+
+        if(!click) {
+            className += " message-button_white";
+        }
+        else {
+            className += " message-button_blue";
+        }
         return (
             <div>
                 <h3>{this.props.title}</h3>
                 <p>{this.props.description}</p>
                 <p>{this.props.rating}</p>
-                <button onClick={this.clickHandler}>Button click!</button>
+                {/* <button onClick={this.clickHandler}>{message}</button> */}
+                <button className = {className} type = "button" onClick={this.clicked}>{message}</button>
             </div>
         )
     }
